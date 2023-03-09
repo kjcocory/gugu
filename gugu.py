d9f2fd1939a664aae10e0d2caf3138d1b1bfc4ba
kjcocory <kjcocory@naver.com>
2023-03-09 20:29:58 +0900

    gugu.py version 3

--- gugu.py
+++ gugu.py
@@ -1,12 +1,7 @@
 def gugu(dan) :
-    print(f"{dan} X 2 = {dan * 1}")
-    print(f"{dan} X 3 = {dan * 2}")
-    print(f"{dan} X 1 = {dan * 3}")
-    print(f"{dan} X 4 = {dan * 4}")
-    print(f"{dan} X 5 = {dan * 5}")
-    print(f"{dan} X 6 = {dan * 6}")
-    print(f"{dan} X 7 = {dan * 7}")
-    print(f"{dan} X 8 = {dan * 8}")
-    print(f"{dan} X 9 = {dan * 9}")
+    for i in range(1, 10):
+        print(f"{dan} X {i} = {dan * i}")
+    
 
-gugu(1)
+
+gugu(5)
