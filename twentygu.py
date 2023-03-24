e9a45da4d606b0d639402962a08901d2db626e74
kjcocory <kjcocory@naver.com>
2023-03-24 13:56:38 +0900

    Feat:twentygu.py

---
+++ twentygu.py
@@ -0,0 +1,10 @@
+def gugu(dan) :
+    for i in range(1, 30):
+        print(f"{dan} X {i} = {dan * i}")
+    
+def total_gugu():
+        for i in range(1, 30):
+            gugu(i)
+            print("----------------------")
+
+total_gugu()
